529aaf3456411bf7e384f61c2809cae052dc6af1
Dreamshaper <tyler.d.hogg@gmail.com>
2018-09-02 01:04:39 -0600

    Day 1 attempt. It's beginning to look like this isn't going to make the deadline for the game jam (I wasted too much time doing other things), but I'm hoping to finish the basics this weekend. Later I will expand on it I think.

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+	public Transform playerLocation;
+
+	private Vector3 original;
+	private float width;
+	
+	// Use this for initialization
+	void Start () {
+		original = transform.position;
+		width = Screen.width;
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () {
+		transform.position = new Vector3(playerLocation.position.x, original.y, original.z);
+	}
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+	// Objects
+	public CharacterController2D controller;
+
+	// Public Variables
+	public float runSpeed = 40f;
+	public float timeBetweenSteps = 0.50f;
+	
+	private float horizontalMovement = 0f;
+	private bool jump = false;
+	private bool crouch = false;
+	
+	private float timePassed = 0f;
+	// Update is called once per frame
+	void Update () {
+		horizontalMovement = Input.GetAxisRaw("Horizontal") * runSpeed;
+	
+		if (Input.GetButtonDown("Jump")) {
+			jump = true;
+		}
+		
+		if (Input.GetButtonDown("Crouch")) {
+			crouch = true;
+		}
+		if (Input.GetButtonUp("Crouch")) {
+			crouch = false;
+		}
+	}
+	
+	void FixedUpdate() {
+		controller.Move(horizontalMovement * Time.fixedDeltaTime, crouch, jump);
+		jump = false;
+		
+		// Walking sounds code
+		if (horizontalMovement != 0) {
+			if (timePassed == 0f) {
+				// Just started walking (or shortly after)
+			}
+			
+			timePassed += Time.fixedDeltaTime;
+			if (timePassed > timeBetweenSteps) {
+				// Has been walking for a bit
+				timePassed = 0f;
+			}
+		}
+		else {
+			// Not walking
+			timePassed = 0f;
+		}
+	}
+}
